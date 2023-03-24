bb8f18163054aeca0d423747569ab678a674935e
ericveganoguera <eveno229@gmail.com>
2023-03-24 15:40:12 +0100

    Add Agents Views and Agents Detail

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use("/", require("./routes/index.routes"));
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/", require("./routes/lineups.routes"));
 app.use("/", require("./routes/agents.routes"));
-app.use("/", require("./routes/lineups.routes"));
+app.use("/", require("./routes/maps.routes"));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/agents.routes.js
+++ routes/agents.routes.js
@@ -5,13 +5,25 @@ const axios = require("axios")
 /* GET home page */
 router.get("/agents", (req, res, next) => {
     axios.get("https://valorant-api.com/v1/agents")
-    .then(res=> {
-        return res.data
+    .then(responseFromAPI=> {
+        res.render("agents/agents",responseFromAPI.data);
     })
-    .then(response=>{
-        res.render("agents/agents",response);
+    .catch(error=>{
+        console.log("Error...: ",error)
+        next()
     })
 });
+
+router.get("/agents/:id", (req,res,next)=>{
+    axios.get(`https://valorant-api.com/v1/agents/${req.params.id}`)
+    .then(responseFromAPI=>{
+        res.render("agents/agents-detail",responseFromAPI.data);
+    })
+    .catch(error=>{
+        console.log("Error...: ",error)
+        next()
+    })
+})
 
 
 module.exports = router;
