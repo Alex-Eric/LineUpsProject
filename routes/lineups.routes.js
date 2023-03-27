f575c1b974bfa4fac9cb508a358126a4a856f04a
ericveganoguera <eveno229@gmail.com>
2023-03-27 09:33:29 +0200

    Add create lineups

--- routes/lineups.routes.js
+++ routes/lineups.routes.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const fileUploader = require('../config/cloudinary.config');
+const Lineup = require("../models/lineups.model")
 
-/* GET home page */
+
 router.get("/lineups", (req, res, next) => {
   res.render("lineups/lineups");
+});
+
+
+router.get("/lineups/create", (req, res, next) => {
+  res.render("lineups/lineups-create");
+});
+
+
+router.post("/lineups/create",fileUploader.single('video-lineup'), (req, res, next) => {
+  const {agent,map,attackDefense} = req.body
+  Lineup.create({video:req.file.path,agent,map,attackDefense})
+  .then(()=>{
+    res.redirect("/lineups")
+  })
+  .catch((error) => {
+    console.log("Error to create lineups..." + error);
+  });
 });
 
 module.exports = router;
